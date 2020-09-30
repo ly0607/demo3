39873e6320f6c51665856a4e6f5506e373d06754
sjh0822 <1847261756@qq.com>
2020-09-30 10:08:59 +0800

    第一次提交盛佳豪

---
+++ src/main/java/com/zoujian/Test.java
@@ -0,0 +1,12 @@
+package com.zoujian;
+
+/**
+ * @Author sjh0822
+ * @Date 2020/9/30 10:06
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
